136ef01b59e84530ffb5d6da2713b898a43bf04e
unknown <racasu86@hotmail.com>
2020-05-25 18:53:14 +0200

    añadidos botones y notas

---
+++ Assets/_Scripts/BtnActivator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BtnActivator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    private bool isActive = false;
+    private Points pointScript;
+
+    private void Start()
+    {
+        pointScript = GameObject.Find("Points").GetComponent<Points>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Note" && isActive) 
+        {
+            pointScript.AddPoints();
+            pointScript.RefreshPoints();
+        }
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isActive = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isActive = false;
+    }
+}
---
+++ Assets/_Scripts/NoteMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMovement : MonoBehaviour
+{
+    private float xPos;
+
+    void Update()
+    {
+        xPos = this.transform.position.x;
+        this.transform.Translate(Vector3.left * 10 * Time.deltaTime);
+
+        if (xPos <= -20.0f) {
+            Destroy(this.gameObject);
+        } 
+    }
+
+}
